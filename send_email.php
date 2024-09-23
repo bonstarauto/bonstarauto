dd28577b2d662e077a2e837e6b952d02a67cb30e
bonstarauto <brittanyssun410@gmail.com>
2024-09-23 20:40:40 +0000

    z

--- send_email.php
+++ send_email.php
@@ -1,15 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize form data
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = "brittanyssun410@gmail.com";
+    // Email details
+    $to = "brittanyssun410@gmail.com"; // Replace with your email
     $subject = "New Contact Form Submission";
     $body = "Name: $name\nEmail: $email\nMessage:\n$message";
 
     $headers = "From: $email";
 
+    // Send email
     if (mail($to, $subject, $body, $headers)) {
         echo "Message sent successfully!";
     } else {
